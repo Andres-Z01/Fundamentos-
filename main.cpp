51d1e33c17a8a26f8eb96ac2fae61dad3d87a6ee
Andres-Z01 <aszambrano8@espe.edu.ec>
2024-11-08 08:58:28 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int suma,A,B;
+    cout << "ingrese el numero A"<<endl;
+    cin >> A;
+    cout << "ingrese el numero B"<<endl;
+    cin >> B;
+    suma = A+B;
+    cout << "el valor de la suma es "<<suma<<endl;
+    cout << "Andres Zambrano!" << endl;
+    return 0;
+}
