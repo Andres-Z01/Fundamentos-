c6ab46a1e3bc3a1c827f2ff230dad6c693296d3e
Andres-Z01 <aszambrano8@espe.edu.ec>
2024-11-13 05:39:41 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   float Gata, Carro, llanta desinflada, llaves, llanta nueva;
+   cout << "colocamos la gata debajo del carro " << endl;
+   cout << "levantamos el carro con la gata " << endl;
+   cout << "con la llave desaflojamos la llanta " << endl;
+   cout << "cambiamos la llanta desinflada por la llanta nueva " << endl;
+   cout << "aseguramos la llanta con la llave " << endl;
+   cout << "al final bajamos el carro " << endl;
+    return 0;
+}
